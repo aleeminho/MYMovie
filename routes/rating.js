d8a3e2566dd03ea197923e34035a8fce984bbb14
aleeminho <a.fikri2411@gmail.com>
2022-07-28 19:10:13 +0700

    add delete feature

--- routes/rating.js
+++ routes/rating.js
@@ -1,11 +1,14 @@
 const router = require('express').Router()
 
-const { getRating, postRating } = require('../handler/rating')
+const { getRating, postRating, deleteRating } = require('../handler/rating')
 
 const { isAuthenticated } = require('../middleware/isAuthenticated')
 
 router.route('/:media?')
   .get(isAuthenticated, getRating)
   .post(isAuthenticated, postRating)
+
+router.route('/:media/delete')
+  .delete(isAuthenticated, deleteRating)
 
 module.exports = router
